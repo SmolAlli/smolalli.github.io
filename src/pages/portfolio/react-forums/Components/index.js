f89856c09eb4c97f1b7771a9630aa23c4896639c
Alli <allikat2706@gmail.com>
2022-12-28 11:16:23 +1100

    Delete src/pages/portfolio directory
    removes this stuff bc it's broken for some reason

--- src/pages/portfolio/react-forums/Components/index.js
+++
@@ -1,7 +0,0 @@
-import Navbar from './Navbar';
-import Footer from './Footer';
-import FrontPage from './FrontPage';
-import Profile from './Profile';
-import Forum from './Forum';
-
-export { Navbar, Footer, FrontPage, Profile, Forum };
--- src/pages/portfolio/react-forums/index.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import App from './App.jsx';
-import { initialise } from './Data/accounts';
-import './index.css';
-
-export default () => {
-    useEffect(() => initialise(), []);
-    return <App />;
-};
